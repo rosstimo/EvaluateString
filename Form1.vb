a62cf4ca19c83748bd0ebb199119576ac4cd39af
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-26 08:33:45 -0600

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim temp As Integer
+        Dim junk As String
+        For Each thingy In TextBox1.Text
+            'junk = CStr(Asc(thingy))
+
+            temp = Asc(thingy)
+
+            Select Case temp
+                Case 48 To 57
+                    MsgBox($"your lucky number is..... {Chr(temp)}")
+                Case Else
+            End Select
+
+            'Try
+            '    temp = CInt(CStr(thingy))
+            '    MsgBox($"your lucky number is..... {temp}")
+            'Catch ex As Exception
+            '    'letter
+
+            'End Try
+        Next
+
+
+    End Sub
+
+
+
+End Class
